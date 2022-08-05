22acc9afff499440f3c16c8a843dd4326cdd7e68
nabin <nkbhaie2017@gmail.com>
2022-08-05 15:41:08 -0600

    add one snapshot test in jest

--- src/Box.test.js
+++ src/Box.test.js
@@ -1,6 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Box from './Box';
 
 test('renders without crashing', () => {
   render(<Box />);
 });
+
+test('it matches snapshot', () => {
+    const {asFragment} = render(<Box />);
+    expect(asFragment()).toMatchSnapshot()
+})
