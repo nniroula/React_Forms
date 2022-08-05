dfc867c7618a8301998c4ca2d727e19d02eb91ed
nabin <nkbhaie2017@gmail.com>
2022-08-05 16:08:45 -0600

    add special tests for getByText for Box component

--- src/Box.test.js
+++ src/Box.test.js
@@ -9,3 +9,10 @@ test('it matches snapshot', () => {
     const {asFragment} = render(<Box />);
     expect(asFragment()).toMatchSnapshot()
 })
+
+// specialized tests, queries tests
+
+test('playing with queries', () => {
+    const { getByText } = render(<Box />);
+    console.log(getByText('Sytling for props goes here'))
+})
